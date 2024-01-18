a4c2e2dffadfdf54f062b0b0d9418e8ee7dce22d
Bogdan <bogdanscherbinadev@gmail.com>
2024-01-18 18:12:06 +0200

    feat: add try to hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createHash } from 'crypto';
+import fs from 'fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+  // const content = await fs.readFile('./src/hash/files/fileToCalculateHashFor.txt', 'utf-8');
+  //
+  // const hex = createHash('sha256').update(content).digest('hex');
+  // console.log(hex);
+
+  const hmac = createHash('sha256');
+
+  const input = fs.createReadStream('./src/hash/files/fileToCalculateHashFor.txt');
+  input.pipe(hmac).pipe(process.stdout);
 };
 
 await calculateHash();
