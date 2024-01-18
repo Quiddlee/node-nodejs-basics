4d3624bb71604e25d02abdc2e4ce65a23be118dd
Bogdan <bogdanscherbinadev@gmail.com>
2024-01-18 16:03:40 +0200

    feat: implement cli/args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
 const parseArgs = () => {
-    // Write your code here 
+  let cache;
+
+  console.log(
+      process.argv
+          .slice(2)
+          .map((value, i) => {
+            if (i % 2 === 0) {
+              cache = value;
+              return;
+            }
+
+            return `${ cache } is ${ value }`;
+          })
+          .filter(Boolean)
+          .join(', ')
+  );
 };
 
 parseArgs();
