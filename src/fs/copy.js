6f3ad96e6cd86d67b98395626c104e2ae01bca72
Bogdan <bogdanscherbinadev@gmail.com>
2024-01-18 14:55:30 +0200

    feat: implement fs/copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,11 @@
+import fs from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+  fs.cp('./src/fs/files', './src/fs/files_copy', { recursive: true, force: false, errorOnExist: true }, (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    }
+  });
 };
 
 await copy();
