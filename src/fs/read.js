9781a1d2e22e8da5ef364333585d6253b4ddc015
Bogdan <bogdanscherbinadev@gmail.com>
2024-01-18 15:12:47 +0200

    feat: implement fs/read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,12 @@
+import fs from 'fs/promises';
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const content = await fs.readFile('./src/fs/files/fileToRead.txt', 'utf-8');
+    console.log(content);
+  } catch (e) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
