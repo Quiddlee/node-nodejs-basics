7ff026841e6a2aed4d25511e0ab454fb29c0c394
Bogdan <bogdanscherbinadev@gmail.com>
2024-01-19 10:56:27 +0200

    feat: implement zip/decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,11 @@
+import { createGunzip } from 'zlib';
+import fs from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+  // TODO - check if right. Should we delete the initial file??
+  fs.createReadStream('./src/zip/files/fileToCompress.txt.gz')
+      .pipe(createGunzip())
+      .pipe(fs.createWriteStream('./src/zip/files/fileToCompress.txt'));
 };
 
 await decompress();
