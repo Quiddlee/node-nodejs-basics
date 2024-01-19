27220021ed1fb9dd0799098b602f649613721c1c
Bogdan <bogdanscherbinadev@gmail.com>
2024-01-19 13:37:45 +0200

    fix: zip solutions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -2,12 +2,9 @@ import { createGzip } from 'node:zlib';
 import fs from 'fs';
 
 const compress = async () => {
-  // TODO - check if right. How to gzip file with changed file name
-
-  const gzip = createGzip();
   fs.createReadStream('./src/zip/files/fileToCompress.txt')
-      .pipe(gzip)
-      .pipe(fs.createWriteStream('./src/zip/files/fileToCompress.txt.gz'));
+      .pipe(createGzip())
+      .pipe(fs.createWriteStream('./src/zip/files/archive.gz'));
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -2,8 +2,7 @@ import { createGunzip } from 'zlib';
 import fs from 'fs';
 
 const decompress = async () => {
-  // TODO - check if right. Should we delete the initial file??
-  fs.createReadStream('./src/zip/files/fileToCompress.txt.gz')
+  fs.createReadStream('./src/zip/files/archive.gz')
       .pipe(createGunzip())
       .pipe(fs.createWriteStream('./src/zip/files/fileToCompress.txt'));
 };
