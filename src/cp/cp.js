687fa10c27bb146aff37a943091ae1ed6a1c2c53
Bogdan <bogdanscherbinadev@gmail.com>
2024-01-19 22:09:15 +0200

    feat: ad cp task solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import { fork } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const SCRIPT_PATH = path.join(__dirname, 'files/script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = fork(SCRIPT_PATH, args, { stdio: [ 'inherit', 'pipe', 'ignore', 'ipc' ] });
+  child.stdout.on('data', (data) => {
+    console.log(`Hello from master process 👋: ${ data.toString() }`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([ 'someArgument1', 'someArgument2' ]);
