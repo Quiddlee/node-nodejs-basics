54aecb603af0c922b2089c526c22c6ac8c0d42c0
Bogdan <bogdanscherbinadev@gmail.com>
2024-01-19 13:52:45 +0200

    fix: calcHash to use streams

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -2,17 +2,12 @@ import { createHash } from 'crypto';
 import fs from 'fs';
 
 const calculateHash = async () => {
-  // TODO - solve this
-
-  // const content = await fs.readFile('./src/hash/files/fileToCalculateHashFor.txt', 'utf-8');
-  //
-  // const hex = createHash('sha256').update(content).digest('hex');
-  // console.log(hex);
-
-  const hmac = createHash('sha256');
-
-  const input = fs.createReadStream('./src/hash/files/fileToCalculateHashFor.txt');
-  input.pipe(hmac).pipe(process.stdout);
+  fs.createReadStream('./src/hash/files/fileToCalculateHashFor.txt')
+      .pipe(
+          createHash('sha256')
+              .setEncoding('hex')
+      )
+      .pipe(process.stdout);
 };
 
 await calculateHash();
