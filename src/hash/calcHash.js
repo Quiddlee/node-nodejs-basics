d823b34c92122155028ec108a6b5c3f533c60552
Bohdan <bogdanscherbinadev@gmail.com>
2024-01-27 18:15:32 +0200

    refactor: change calc hash chaining

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -3,10 +3,13 @@ import fs from 'fs';
 
 const calculateHash = async () => {
   fs.createReadStream('./src/hash/files/fileToCalculateHashFor.txt')
+      .on('error', () => {
+        throw new Error('FS operation failed');
+      })
       .pipe(
           createHash('sha256')
-              .setEncoding('hex')
       )
+      .setEncoding('hex')
       .pipe(process.stdout);
 };
 
