b741b14e13262c97c65a196d59b94b424e208462
Bohdan <bogdanscherbinadev@gmail.com>
2024-01-27 18:05:55 +0200

    refactor: remove comments

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -12,5 +12,4 @@ const spawnChildProcess = async (args) => {
   });
 };
 
-// Put your arguments in function call to test this functionality
 spawnChildProcess([ 'someArgument1', 'someArgument2' ]);
