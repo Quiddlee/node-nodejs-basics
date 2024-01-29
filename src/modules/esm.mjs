cc929a76ce430f2a64beb8987282b838f5cc9496
Bohdan <bogdanscherbinadev@gmail.com>
2024-01-29 11:53:42 +0200

    fix: modules file import

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,10 +1,9 @@
-import path from 'path';
-import { release, version } from 'os';
 import { createServer as createServerHttp } from 'http';
+import { release, version } from 'os';
+import path from 'path';
 import { fileURLToPath } from 'url';
-import { readFile } from 'fs/promises';
 
-'./files/c.js';
+import "./files/c.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
